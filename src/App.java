b6c94aaaf0a38e464f5d902d584f37341f243295
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-09-08 19:56:23 +0200

    Update code at school workshop

--- src/App.java
+++ src/App.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+        final FileHandler fileHandler = new FileHandler();
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Skriv in vad du vill kryptera: ");
-        String encryptedText = scanner.nextLine();
+        String inputText = scanner.nextLine();
 
         Encrypter encrypter = new Encrypter();
-        String encryptedText = encrypter.encryptText('j', 'k');
-        System.out.println("Här är din krypterade text: " + encryptedText);
+        String encryptedText = encrypter.encryptText('j', 'k', inputText);
+        System.out.println(
+                "Här är din originala text: " + inputText + " och här är din krypterade text: " + encryptedText);
         scanner.close();
+        fileHandler.writeFile(encryptedText);
     }
-
 }
--- src/Encrypter.java
+++ src/Encrypter.java
@@ -3,8 +3,9 @@ package src;
 public class Encrypter {
   private String text;
 
-  public String encryptText(char oldChar, char newChar) {
+  public String encryptText(char oldChar, char newChar, String wholeWord) {
+    text = wholeWord;
     text = text.replace(oldChar, newChar);
     return text;
- }
+  }
 }
